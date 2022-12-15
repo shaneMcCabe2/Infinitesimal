f3992360108ce1df1ec366d2511fe233bff58c5c
Shane <shane.mccabe27@gmail.com>
2022-12-15 09:26:07 -0500

    added flask shell context processor

--- infinitesimal.py
+++ infinitesimal.py
@@ -1,1 +1,6 @@
-from app import app # define flask application instance
+from app import app, db # define flask application instance
+from app.models import Contact
+
+@app.shell_context_processor # pre-import the application instance and arguments provided
+def make_shell_context():
+    return {'db': db, 'Contact': Contact}
